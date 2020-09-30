5335d8520e715d6aee90bfb5aa46ede12cd9a45f
thalycomp <thalycomp@gmail.com>
2020-09-30 13:29:57 -0300

    definindo estilo css global

---
+++ src/style/global.js
@@ -0,0 +1,30 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+    @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100;400&display=swap');
+    * {
+        margin: 0;
+        padding: 0;
+        outline: 0;
+        box-sizing: border-box;
+        -webkit-box-sizing: border-box;
+        -moz-box-sizing: border-box;
+    }
+    :root {
+        --verde: #02BE6E;
+        --cinza:  #F0F1F8;
+    }
+    body {
+      -webkit-font-smoothing: antialiased;
+    }
+    body, input, button {
+        font: 14px Roboto, sans-serif;
+    }
+    #root {
+        margin: 0 auto;
+        padding: 0 20px 50px;
+    }
+    button {
+        cursor: pointer;
+    }
+`;
